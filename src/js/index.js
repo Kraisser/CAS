49515a6ea61234f70a32736d4be462bff0848a5d
Kraisser <vgaiko@mail.ru>
2024-08-17 17:31:39 +0300

    1.0

---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+import 'normalize.css';
+
+import '../scss/common/common.scss';
+
+import '../scss/welcome-logo.scss';
+import '../scss/cases.scss';
+import '../scss/header.scss';
+import '../scss/footer.scss';
